5f00b497fff40839e8fc7c050d57c3c9cd8ae5f7
Novianto1931037 <ongnovianto@gmail.com>
2021-10-11 18:20:43 +0700

    pertambahan Settings & Personal Data Page

---
+++ app/src/main/java/com/example/mobile_application/ui/settings/PersonalDataViewModel.java
@@ -0,0 +1,7 @@
+package com.example.mobile_application.ui.settings;
+
+import androidx.lifecycle.ViewModel;
+
+public class PersonalDataViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/mobile_application/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/mobile_application/ui/settings/SettingsFragment.java
@@ -1,32 +1,30 @@
-package com.example.mobile_application.ui.notifications;
+package com.example.mobile_application.ui.settings;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.mobile_application.R;
 import com.example.mobile_application.databinding.FragmentSettingsBinding;
 
-public class NotificationsFragment extends Fragment {
+public class SettingsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private SettingsViewModel settingsViewModel;
     private FragmentSettingsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
 
 /*        final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
