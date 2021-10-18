20869ce1c679376341d5b0ebe433d891421ffcf9
Novianto1931037 <ongnovianto@gmail.com>
2021-10-18 18:38:19 +0700

    pertambahan Task & Task Detail Page

---
+++ app/src/main/java/com/example/mobile_application/ui/task/TaskDetailViewModel.java
@@ -0,0 +1,7 @@
+package com.example.mobile_application.ui.task;
+
+import androidx.lifecycle.ViewModel;
+
+public class TaskDetailViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/mobile_application/ui/task/TaskLogViewModel.java
@@ -0,0 +1,7 @@
+package com.example.mobile_application.ui.task;
+
+import androidx.lifecycle.ViewModel;
+
+public class TaskLogViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
