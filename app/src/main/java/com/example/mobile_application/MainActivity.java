34c75bac4af52ad1e5254d0ea5b6aa33c8496a53
Asus <hengky9099@gmail.com>
2021-12-15 19:22:49 +0700

    desain ubah

--- app/src/main/java/com/example/mobile_application/MainActivity.java
+++ app/src/main/java/com/example/mobile_application/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Objects.requireNonNull(getSupportActionBar()).hide();
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
